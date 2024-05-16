27218f9544ccd92f18bc39e6d90aaacd56cd3147
jaceleedev <jaceleedev@gmail.com>
2024-05-16 19:30:28 +0900

    feat: :technologist: complete chunk

---
+++ easy/chunk.js
@@ -0,0 +1,15 @@
+/**
+ * @template T
+ * @param {Array<T>} array The array to process.
+ * @param {number} [size=1] The length of each chunk.
+ * @returns {Array<Array<T>>} The new array of chunks.
+ */
+export default function chunk(array, size = 1) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+
+  return result;
+}
