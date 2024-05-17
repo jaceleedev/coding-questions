692717e96ac4c7b348e8c5927dcab8f635486bbb
jaceleedev <jaceleedev@gmail.com>
2024-05-17 21:24:49 +0900

    feat: :technologist: complete drop-while

---
+++ easy/drop-while.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @return {Array} Returns the slice of `array`.
+ */
+export default function dropWhile(array, predicate) {
+  let i = 0;
+
+  while (i < array.length && predicate(array[i], i, array)) {
+    ++i;
+  }
+
+  return array.slice(i);
+}
