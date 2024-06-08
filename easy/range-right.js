12a81a97f0b226bd556c9c293a922aa10837685b
jaceleedev <jaceleedev@gmail.com>
2024-06-08 16:37:57 +0900

    feat: :technologist: complete range-right

---
+++ easy/range-right.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Number} start - The first number of the resultant array.
+ * @param {Number} end - The value where the resultant array will stop at and not contain it.
+ * @param {Number} step - The step / increment value of each number in the array.
+ * @return {Array<Number>} Returns the array with the sequence of numbers in the specified range.
+ */
+export default function rangeRight(start = 0, end, step = 1) {
+  if (end === undefined) {
+    end = start;
+    start = 0;
+  }
+
+  if (start > end && step === 1) {
+    step = -1;
+  }
+
+  const length = (end - start) / (step || 1);
+
+  const result = [];
+
+  for (let i = 0; i < length; ++i) {
+    result.push(start + step * i);
+  }
+
+  return result.reverse();
+}
